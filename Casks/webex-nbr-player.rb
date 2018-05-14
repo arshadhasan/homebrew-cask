34895992aea742b7d800e0f5f125d4ac45d38dc2
Arshad Hasan <arshadhasan@outlook.com>
2018-05-15 02:34:49 +0530

    Update webex-nbr-player.rb sha256
    Update webex-nbr-player.rb

--- Casks/webex-nbr-player.rb
+++ Casks/webex-nbr-player.rb
@@ -1,6 +1,6 @@
 cask 'webex-nbr-player' do
   version 'T31L'
-  sha256 '76be3ec4e8d8c7d7efab8b6900e8d3d1e933faa9e9587293547d612c5b34b311'
+  sha256 '770047eff6e0deb7b1ce6c8e3ab1d3394ff60a183d2ad55d3fdb48a6812e5920'
 
   url "https://welcome.webex.com/client/#{version}/mac/intel/webexnbrplayer_intel.dmg"
   name 'Webex Network Recording player'
